820157c08d3c8335b52356c6b6bb58556126ec2b
Nicolas <nicolasvogler0@gmail.com>
2023-01-07 18:54:15 -0300

    add directory for gh pages

--- src/test/java/runner/MainRunner.java
+++ src/test/java/runner/MainRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber.html"},
+        plugin = {"pretty", "html:target/cucumber/cucumber.html"},
         features = {"classpath:features/"},
         glue = {"stepDefinitions"},
         tags = "@smoke"
